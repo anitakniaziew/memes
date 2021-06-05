89ae27609bc79b1aa6901c5db6d86fe25fe3f27c
Anita Kowalska <anita.kniaziew@gmail.com>
2021-06-06 00:04:09 +0200

    Add prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
+import Main from './components/Main/Main';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
@@ -16,7 +14,7 @@ function App() {
           <Navbar />
           <Main />
         </div>
-      </ Router>
+      </Router>
     </Provider>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,23 +1,23 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-import memes from './memes'
+import memes from './memes';
 
-const initialState =  memes;
+const initialState = memes;
 
 const slice = createSlice({
   name: 'memes',
   initialState,
   reducers: {
     upvote(state, action) {
-      state.find(item => item.id === action.payload).upvotes++;
+      state.find((item) => item.id === action.payload).upvotes++;
     },
     downvote(state, action) {
-      state.find(item => item.id === action.payload).downvotes++;
-    }
-  }
+      state.find((item) => item.id === action.payload).downvotes++;
+    },
+  },
 });
 
-const store = configureStore({reducer: { memes: slice.reducer}});
+const store = configureStore({ reducer: { memes: slice.reducer } });
 
 export const { upvote, downvote } = slice.actions;
 export default store;
