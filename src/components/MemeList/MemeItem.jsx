b18e5f95d0f0a42b3f533678116c999d087c6567
Anita Kowalska <anita.kniaziew@gmail.com>
2021-06-06 00:24:24 +0200

    Style MemeList and MemeItem

---
+++ src/components/MemeList/MemeItem.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import classnames from 'classnames';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { downvote, upvote } from '../../store';
+import styles from './memeItem.module.scss';
+
+const MemeItem = ({ id, img, title, downvotes, upvotes, type }) => {
+  const [isFav, setIsFav] = useState(false);
+  const dispatch = useDispatch();
+
+  return (
+    <div className={styles.memeItemContainer}>
+      <div className={styles.memeTitle}>
+        <h2>{title}</h2>
+        <StarBorderIcon
+          className={classnames([styles.favBtn, { [styles.active]: isFav }])}
+          onClick={() => setIsFav(!isFav)}
+        />
+      </div>
+      <img src={img} alt="meme" className={styles.memeImg} />
+      <div className={styles.voteBtnsContainer}>
+        <button
+          type="button"
+          onClick={() => dispatch(upvote(id))}
+          className={classnames([
+            styles.voteBtn,
+            { [styles.hot]: type === 'hot' },
+          ])}
+        >
+          <ArrowUpwardIcon /> {upvotes}
+        </button>
+        <button
+          type="button"
+          onClick={() => dispatch(downvote(id))}
+          className={classnames([
+            styles.voteBtn,
+            { [styles.regular]: type === 'regular' },
+          ])}
+        >
+          <ArrowDownwardIcon />
+          {downvotes}
+        </button>
+      </div>
+    </div>
+  );
+};
+MemeItem.propTypes = {
+  id: PropTypes.number,
+  img: PropTypes.string,
+  title: PropTypes.string,
+  downvotes: PropTypes.number,
+  upvotes: PropTypes.number,
+};
+export default MemeItem;
---
+++ src/components/MemeList/MemeList.jsx
@@ -0,0 +1,25 @@
+import MemeItem from './MemeItem';
+import styles from './memeList.module.scss';
+
+const MemeList = ({ memes, type }) => {
+  if (memes.length === 0)
+    return <h1>There're no memes in this category yet!</h1>;
+
+  return (
+    <div className={styles.memeList}>
+      {memes.map(({ id, title, img, downvotes, upvotes }) => (
+        <MemeItem
+          id={id}
+          key={id}
+          title={title}
+          img={img}
+          downvotes={downvotes}
+          upvotes={upvotes}
+          type={type}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MemeList;
