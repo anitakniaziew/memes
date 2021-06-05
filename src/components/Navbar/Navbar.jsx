226724d5502e3969885beb87684b28621ce95deb
Anita Kowalska <anita.kniaziew@gmail.com>
2021-06-06 00:22:27 +0200

    Style Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import { Link, useRouteMatch } from 'react-router-dom';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import classnames from 'classnames';
+import styles from './navbar.module.scss';
+
+const Navbar = () => {
+  const hot = useRouteMatch('/hot');
+  const regular = useRouteMatch('/regular');
+
+  return (
+    <nav className={styles.header}>
+      <Tabs orientation="vertical">
+        <Tab
+          icon={<WhatshotIcon />}
+          label="hot"
+          component={Link}
+          to="/hot"
+          className={classnames([styles.navLink, { [styles.active]: hot }])}
+        />
+        <Tab
+          icon={<AcUnitOutlinedIcon />}
+          label="regular"
+          component={Link}
+          to="/regular"
+          className={classnames([styles.navLink, { [styles.active]: regular }])}
+        />
+      </Tabs>
+      <small>Copywright &copy; Anita Kowalska</small>
+    </nav>
+  );
+};
+
+export default Navbar;
