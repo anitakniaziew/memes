6d8af01010fc4270ac5a6e5a430217a754aa44bd
Anita Kowalska <anita.kniaziew@gmail.com>
2021-06-06 00:20:21 +0200

    Convert css files into scss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Main from './components/Main/Main';
 import Navbar from './components/Navbar/Navbar';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
