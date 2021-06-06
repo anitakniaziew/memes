7ed73750c634c5c0d64d648f85b6c65c7693397c
Anita Kowalska <anita.kniaziew@gmail.com>
2021-06-06 10:04:18 +0200

    Add braces

--- src/components/MemeList/MemeList.jsx
+++ src/components/MemeList/MemeList.jsx
@@ -2,8 +2,9 @@ import MemeItem from './MemeItem';
 import styles from './memeList.module.scss';
 
 const MemeList = ({ memes, type }) => {
-  if (memes.length === 0)
+  if (memes.length === 0) {
     return <h1>There're no memes in this category yet!</h1>;
+  }
 
   return (
     <div className={styles.memeList}>
