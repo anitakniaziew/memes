6a99d611c1357edbe6b9d6f9127961a6da4b06ef
Anita Kowalska <anita.kniaziew@gmail.com>
2021-06-06 08:27:37 +0200

    Fix navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,4 @@
 import { Link, useRouteMatch } from 'react-router-dom';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
 import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import classnames from 'classnames';
@@ -12,22 +10,20 @@ const Navbar = () => {
 
   return (
     <nav className={styles.header}>
-      <Tabs orientation="vertical">
-        <Tab
-          icon={<WhatshotIcon />}
-          label="hot"
-          component={Link}
+      <ul className={styles.navLinks}>
+        <Link
           to="/hot"
           className={classnames([styles.navLink, { [styles.active]: hot }])}
-        />
-        <Tab
-          icon={<AcUnitOutlinedIcon />}
-          label="regular"
-          component={Link}
+        >
+          <WhatshotIcon /> Hot
+        </Link>
+        <Link
           to="/regular"
           className={classnames([styles.navLink, { [styles.active]: regular }])}
-        />
-      </Tabs>
+        >
+          <AcUnitOutlinedIcon /> Regular
+        </Link>
+      </ul>
       <small>Copywright &copy; Anita Kowalska</small>
     </nav>
   );
